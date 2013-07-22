bf2ebb1025b7bbaabfe612eab58c14f3e3265c22
Aaron <almax27@hotmail.co.uk>
2013-07-22 16:08:28 +0100

    Added first pass AI in XY plane
    Added some placeholder and test assets

---
+++ Assets/Code/Scripts/PlayerControl.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerControl : MonoBehaviour {
+	
+	#region public variables
+	
+	List<AIPathXY> selectedUnits = new List<AIPathXY>();
+	
+	#endregion
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
